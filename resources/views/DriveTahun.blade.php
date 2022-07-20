89ebe04452c1023b70b1cd138b3374107060be26
Alifan12 <alif129310@gmail.com>
2022-07-20 08:26:01 +0700

    input bootstrap 5

---
+++ resources/views/DriveTahun.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistem File Manager | Kemendagri</title>
+  <link rel="stylesheet" href="bootstrap-5.2.0-dist/css/bootstrap.min.css">
+</head>
+
+<body>
+  <nav>
+    <img src="" alt="">
+  </nav>
+  <script src="bootstrap-5.2.0-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
