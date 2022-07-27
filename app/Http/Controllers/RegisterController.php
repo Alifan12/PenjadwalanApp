11007b12b67e0499fd0319d5fdbe04a87139a1c3
YulioAdePranata <yulioade0712@gmail.com>
2022-07-27 09:52:30 +0700

    update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,9 +7,20 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     public function index(){
-        return view('register.index', [
+        return view('register.Register', [
             'title' => 'Register',
             'active' => 'register'
+        ]);
+    }
+    public function store(Request $request){
+        $request->validate([
+            'nama'=>'required|max:255',
+            'nip'=>'required|max:255',
+            'alamat'=>'reqired|max:255',
+            'namaunitkerja'=>'required|max:255',
+            'email'=>'required|email|max:255|unique:user',
+            'telepon'=>'required|max:255',
+            'password'=>'required|min:8|max255|unique:user'
         ]);
     }
 }
